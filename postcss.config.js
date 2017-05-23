c5aee62a8329c54039d191a12f9caca41a2f0f79
qinmingyuan <mingyuan0715@foxmail.com>
2017-05-23 18:30:04 +0800

    ordered all rules

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,42 @@
 module.exports = {
   plugins: [
-    require('cssnano')({core: true, reduceDisplayValues: true, discardDuplicates: true}),
+    require('cssnano')({
+      discardComments: true,
+      minifyGradients: true,
+      reduceInitial: true,
+      svgo: true,
+      reduceDisplayValues: true,
+      reduceTransforms: true,
+      autoprefixer: true,
+      zindex: true,
+      convertValues: true,
+      reduceTimingFunctions: true,
+      calc: true,
+      colormin: true,
+      orderedValues: true,
+      minifySelectors: true,
+      minifyParams: true,
+      normalizeCharset: true,
+      discardOverridden: true,
+      normalizeString: true,
+      normalizeUnicode: true,
+      minifyFontValues: true,
+      discardUnused: true,
+      normalizeUrl: true,
+      functionOptimiser: true,
+      filterOptimiser: true,
+      reduceBackgroundRepeat: true,
+      reducePositions: true,
+      core: true,
+      mergeIdents: true, //unsafe
+      reduceIdents: true, //unsafe
+      mergeLonghand: true,
+      discardDuplicates: false,
+      mergeRules: true,
+      discardEmpty: true,
+      uniqueSelectors: false,
+      styleCache: true
+    }),
     //require('postcss-discard-duplicates')({discardDuplicates: false})
   ]
 }

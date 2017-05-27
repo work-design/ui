37155b17a30dbcf982d6fba8ad96ec0bfd1ae47d
qinmingyuan <mingyuan0715@foxmail.com>
2017-05-27 15:05:59 +0800

    use cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     require('cssnano')({
       filterPlugins: true,
-      discardComments: true,
+      discardComments: { removeAll: true },
       minifyGradients: true,
       reduceInitial: true,
       svgo: true,

e203a84d7d5e7423717e8f36ee57de34412dec7d
qinmingyuan <mingyuan0715@foxmail.com>
2020-01-17 23:13:20 +0800

    custom config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,5 +3,9 @@ const { resolve } = require('path')
 
 const env = environment.toWebpackConfig()
 env.resolve.modules = env.resolve.modules.concat(resolve('node_modules'))
+env.output = {
+  filename: '[name].js',
+  publicPath: 'dist'
+}
 debugger
 module.exports = env

40296364806913380ecf0941f2b799d359ef1194
qinmingyuan <mingyuan0715@foxmail.com>
2020-01-18 00:14:07 +0800

    add index;

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,9 +3,9 @@ const { resolve } = require('path')
 
 const env = environment.toWebpackConfig()
 env.resolve.modules = env.resolve.modules.concat(resolve('node_modules'))
-env.output = {
-  filename: '[name].js',
-  publicPath: 'dist'
-}
+
+env.output = Object.assign(env.output, {
+  filename: '[name].js'
+})
 debugger
 module.exports = env

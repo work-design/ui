293abbb4ed9068ff9f2df58bd56141bea3789d4f
qinmingyuan <mingyuan0715@foxmail.com>
2020-01-21 23:34:06 +0800

    generated semantic css

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,15 @@
 const { environment } = require('@rails/webpacker')
 const { resolve } = require('path')
+const less = require('./loaders/less')
+
+environment.loaders.prepend('less', less)
 
 const env = environment.toWebpackConfig()
 env.resolve.modules = env.resolve.modules.concat(resolve('node_modules'))
-
+const x = {
+  semantic: './assets/semantic.less'
+}
+env.entry = Object.assign(env.entry, x)
 env.output = Object.assign(env.output, {
   filename: '[name].js'
 })
